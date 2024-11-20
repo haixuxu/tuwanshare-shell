a1592c19179bccd13f1cab87b3ced08ce6a9dac2
xuxihai <xuxh@tuwan.com>
2024-11-20 10:17:24 +0800

    remove vite

--- upload2test.sh
+++
@@ -1,3 +0,0 @@
-scp ./src/renderer/dist/index.html root@192.168.0.35:/home/web/website/tuwan_www/templets/static/play/diandianele.htm
-
-#cp -rf ./src/renderer/dist/assets/* ~/taole/Static/diandianele/assets
